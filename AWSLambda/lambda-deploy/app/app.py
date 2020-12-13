3b01c51497878310e820479692d9d1654e3df417
cameronsmith <cameron.smith@davanti.co.nz>
2020-12-13 14:03:30 +1300

    First successful execution of lambda

--- AWSLambda/lambda-deploy/app/app.py
+++ AWSLambda/lambda-deploy/app/app.py
@@ -13,16 +13,11 @@ from PIL import Image
 def handler(event, context):
 
     request_body = event
-    #image = io.BytesIO(base64.b64decode(request_body['image']))
-    #text = request_body['template']
     
     template = request_body['template']
     pdfFilesArray = request_body['pdffiles']
     labelsArray = template['labels']
 
-    #Make a temporary folder to store the pdf when they are converted from base64, delete the directory after execition for cleanup
-    #tempFolderName = "/tmp"
-    #os.makedirs(tempFolderName)
 
     allExtractedInvoiceData = [] #Array to store all the extracted data from all invoices
     counter = 0 #Used for naming image files
@@ -47,30 +42,24 @@ def handler(event, context):
 
         pages = pdf2image.convert_from_path(filename, 500)
         for page in pages:
-            print('Looping over a page')
             page.save('/tmp/' +str(counter) + '.jpg', 'JPEG')
-            print('The file name is')
             print('/tmp/' +str(counter) + '.jpg')
             #This is really bad but using counter to create unique names TODO handle multi page pdf
             counter += 1
+            #This will only extract the first page of the invoice TODO work with multipage pdfs
+            break
+
 
             #File cleanup delete pdf
         if os.path.exists(filename):
             print('Deleting file' + filename)
             os.remove(filename)
 
-
-
-
-
-#Code below iterates over all the invoices that have been converted to jpg and dumps into tempFolderName directory
+    #Code below iterates over all the invoices that have been converted to jpg and dumps into tempFolderName directory
     images = glob.glob("/tmp/*.jpg") #Get a collection of all files in the temp folder
-    print('Reading glob for images in tmp directory')
     for img in images:
-        
+       
         extractedFileDate = {} #A dict that will store all the data extracted from 1 file
-            #invoice = Image.open(file) #Invoice variable is the individual invoice we are working with
-           # invoice = image.resize((1653,2339),Image.ANTIALIAS) # TODO figure out the dimensions of the image
             
         invoice = cv2.imread(img)
         invoice = cv2.resize(invoice, (1653,2339), interpolation=cv2.INTER_AREA)
@@ -84,13 +73,13 @@ def handler(event, context):
 
             crop_img = invoice[y:y+h, x:x+w]
             gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-
             #create a temp file from the extracted region
-            filename = "{}.jpg".format(os.getpid())
+            filename = "/tmp/{}.jpg".format(secrets.token_urlsafe(8))
             cv2.imwrite(filename, gray)
 
             #extracted text from the specified region
-            extractedText = pytesseract.image_to_string(cv2.imread(filename))
+            extractedText = pytesseract.image_to_string(Image.open(filename))
+            print(extractedText)
             extractedFileDate[lblName] = extractedText #Let the extracted next into the dict
             #Take all the data extracted from a single invoice and insert it as a record to allExtractedInvoiceData array
         allExtractedInvoiceData.append(extractedFileDate)
@@ -98,7 +87,6 @@ def handler(event, context):
     #Outside of all the loops
     #sets the output to extracted data array
     output = allExtractedInvoiceData
-
 
 
     body = {
--- AWSLambda/lambda-deploy/deploy.sh
+++ AWSLambda/lambda-deploy/deploy.sh
@@ -1,3 +1,5 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 945616793337.dkr.ecr.us-east-1.amazonaws.com/invoiceocrrepo
+
 docker build -t invoiceocrcontainer .
 docker tag invoiceocrcontainer:latest 945616793337.dkr.ecr.us-east-1.amazonaws.com/invoiceocrrepo:latest2
 docker push 945616793337.dkr.ecr.us-east-1.amazonaws.com/invoiceocrrepo:latest2
